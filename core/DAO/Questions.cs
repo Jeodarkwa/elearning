78b8390e988dbcda345ae97aed06ade466b82e3d
Jeodarkwa <Jeodarkwa@yahoo.com>
2015-08-13 10:32:18 +0200

    questions file

---
+++ core/DAO/Questions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CORE.DAO
+{
+    public class Questions
+    {
+        public Int16 Question_ID { get; set; }
+        public String Examquestion { get; set; }
+        public String solution1 { get; set; }
+        public String solution2 { get; set; }
+        public String solution3 { get; set; }
+        public Char answer { get; set; }
+
+    }
+}
